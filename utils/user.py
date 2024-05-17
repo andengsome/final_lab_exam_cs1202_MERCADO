04182fe2d8bebc44b893d34410d083afdb9e48e8
andengsome <23-05956@g.batstate-u.edu.ph>
2024-05-17 09:34:22 +0800

    update

--- utils/user.py
+++ utils/user.py
@@ -1,2 +1,6 @@
 class User:
-	pass
+    def __init__(self, username, password, points=0, stages_won=0):
+        self.username = username
+        self.password = password
+        self.points = points
+        self.stages_won = stages_won
